53cf973bc99b685e41f5cd425a90613d749b3b37
WoshiWoshu <gabriel.pironneau@epitech.eu>
2020-05-01 17:02:56 +0200

    [INIT] Architecture + DB + Small Documentation added

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+const app = express();
+
+mongoose.connect('mongodb+srv://titi8291:amazeio9@cluster0-npk8d.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Successfully connected to MongoDB Atlas !');
+})
+.catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas !');
+    console.error(error);
+});
+
+app
+.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
+.use(bodyParser.json());
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
