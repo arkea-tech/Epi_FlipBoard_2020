d9047d3cf09d09f90885557e0652485b45c0173d
WoshiWoshu <gabriel.pironneau@epitech.eu>
2020-05-07 16:20:20 +0200

    [CHORE] web_client

---
+++ web_client/Screens/Login.js
@@ -0,0 +1,67 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import {ImageBackground, Image, Button, View, StyleSheet, SafeAreaView, Text, StatusBar} from 'react-native';
+
+
+export function LoginScreen({navigation}) {
+    return (
+      
+      <View style={styles.container}>
+      <Text style= {styles.centered}> this is login screen </Text>
+
+      <Button mode="contained" onPress={() => navigation.navigate('Spotlight')}
+          title= "Explore Spotlight"
+          style={styles.buttoncontainer}
+      />
+      <Button mode="contained" onPress={() => navigation.navigate('Featured')}
+          title= "Featured"
+          style={styles.buttoncontainer}
+      />
+      <Button mode="contained" onPress={() => navigation.navigate('News')}
+          title= "News"
+          style={styles.buttoncontainer}
+      />
+      <Button mode="contained" onPress={() => navigation.navigate('Business')}
+          title= "Business"
+          style={styles.buttoncontainer}
+      />
+      <Button mode="contained" onPress={() => navigation.navigate('Tech')}
+          title= "Tech and science"
+          style={styles.buttoncontainer}
+      />
+      <Button mode="contained" onPress={() => navigation.navigate('Sport')}
+          title= "Sport"
+          style={styles.buttoncontainer}
+      />
+      </View>
+    )
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'flex-end',
+      justifyContent: 'space-evenly',
+    },
+    buttonContainer: {
+      width: 100, 
+      height: 100,
+    },
+    centered: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    background: {
+      width: null,
+      height: null,
+      flex: 1,
+    },
+    title: {
+      justifyContent: 'center'
+    }
+  
+  });
+ 
