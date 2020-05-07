c0537cecbceadb85901b4d8e72b3690d71056b93
Warren-O <64646757+Warren-O@users.noreply.github.com>
2020-05-07 16:17:01 +0200

    Added Navigate

---
+++ Screens/Featured.js
@@ -0,0 +1,66 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import {Button, View, StyleSheet,Text} from 'react-native';
+
+export function FeaturedScreen({navigation}) {
+    return (
+      
+      <View style={styles.container}>
+      <Text style= {styles.centered}> this is featured screen </Text>
+
+      <Button mode="contained" onPress={() => navigation.navigate('Spotlight')}
+          title= "Explore Spotlight"
+          style={styles.buttoncontainer}
+      />
+      <Button mode="contained" onPress={() => navigation.push('Featured')}
+          title= "Featured"
+          style={styles.buttoncontainer}
+      />
+      <Button mode="contained" onPress={() => navigation.navigate('News')}
+          title= "News"
+          style={styles.buttoncontainer}
+      />
+      <Button mode="contained" onPress={() => navigation.navigate('Business')}
+          title= "Business"
+          style={styles.buttoncontainer}
+      />
+      <Button mode="contained" onPress={() => navigation.navigate('Tech')}
+          title= "Tech and science"
+          style={styles.buttoncontainer}
+      />
+      <Button mode="contained" onPress={() => navigation.navigate('Sport')}
+          title= "Sport"
+          style={styles.buttoncontainer}
+      />
+      </View>
+    )
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'flex-end',
+      justifyContent: 'space-evenly',
+    },
+    buttonContainer: {
+      width: 100, 
+      height: 100,
+    },
+    centered: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    background: {
+      width: null,
+      height: null,
+      flex: 1,
+    },
+    title: {
+      justifyContent: 'center'
+    }
+  
+  });
+  
