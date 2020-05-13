b58da1f26f8ee909eb1be10ffde838eae65aedaa
hugo.lacour <hugo.lacour@epitech.eu>
2020-05-13 15:23:39 +0200

    send result

--- server/routes/stuff.js
+++ server/routes/stuff.js
@@ -17,15 +17,32 @@ router.delete('/:id', auth, stuffCtrl.deleteThing);
 router.post('/news', (req, res) => {
     const json_request_body = JSON.parse(JSON.stringify(req.body));
     const word = json_request_body.word
-
+    var arr = []
+    var len = 0
     const options = {
-        url: 'https://newsapi.org/v2/everything?q=' + word + '&sortBy=popularity&country=fr&apiKey=bd1876640c824a3ca28a2d6d16b93cd7' ,
+        url: 'https://newsapi.org/v2/everything?q=' + word + '&sortBy=popularity&apiKey=bd1876640c824a3ca28a2d6d16b93cd7' ,
         method: 'GET'
     }
 
     request(options, function(err, resB, body) {
         let json = JSON.parse(body);
-        res.send(json)
+
+        len = json.articles.length
+        for (var i = 0; i < len; i++) {
+            arr.push({
+                id: i + 1,
+                Tags: word,
+                Title: json.articles[i].title,
+                Link: json.articles[i].url,
+                Image: json.articles[i].urlToImage,
+                Desc: json.articles[i].description,
+                Source: json.articles[i].source.name,
+                Author: json.articles[i].author,
+                Date: json.articles[i].publishedAt,
+                Content: json.articles[i].content
+            });
+        }
+        res.send(arr)
     });
 });
 
