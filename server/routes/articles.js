3a7f4633721797047bdc03dcda440d18b3a90fe3
WoshiWoshu <gabriel.pironneau@epitech.eu>
2020-05-15 04:40:24 +0200

    [HOTFIX] Optimized execution calls

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -5,9 +5,5 @@ const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/:section', articlesCtrl.getArticles);
-// router.post('/', auth, nytCtrl.createThing);
-// router.get('/:id', auth, nytCtrl.getOneThing);
-// router.put('/:id', auth, nytCtrl.modifyThing);
-// router.delete('/:id', auth, nytCtrl.deleteThing);
 
 module.exports = router;
