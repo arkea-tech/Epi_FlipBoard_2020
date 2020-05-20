d6a7e81ed725a5f4cf1f0de61b27451dd90fc6e1
WoshiWoshu <gabriel.pironneau@epitech.eu>
2020-05-20 02:02:02 +0200

    [ADD] Popular Routes Created

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const articlesCtrl = require('../controllers/articles');
+const articlesPopularCtrl = require('../controllers/articles');
 const auth = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/:section', auth, articlesCtrl.getArticles);
+router.get('/popular', auth, articlesCtrl.getPopularArticles);
 
 module.exports = router;
