cfda1c99988ef65956e8b14b05c8195c27051d72
WoshiWoshu <gabriel.pironneau@epitech.eu>
2020-05-20 02:08:49 +0200

    [ADD] Controllers+ logic implemented

---
+++ server/controllers/popular_articles.js
@@ -0,0 +1,24 @@
+var axios = require('axios');
+//let parseArticles = require('../parsing/parse_articles');
+
+exports.getArticles = (req, res, next) => {
+    let articles = [];
+    let id = 0;
+
+    axios.all([
+        axios.get(`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=SwuUJu3Hgbrr1qZDHCZzR4trZod9998z`),
+        axios.get(`https://content.guardianapis.com/search?api-key=0dbaf7e0-8c75-4864-82ed-34fc14798075&show-fields=thumbnail,headline,trailText,publication,byline,lastModified&order-by=relevance`)
+    ]).then(
+        axios.spread((new_york_times, the_guardian) => {
+            // id = parseArticles.parseNytDatas(articles, new_york_times, id, `${section.new_york_times}`);
+            // parseArticles.parseGuardianDatas(articles, the_guardian, id, `${section.new_york_times}`);
+            // res.status(200).json(articles);
+        })
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+}
--- server/routes/articles.js
+++ server/routes/articles.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const articlesCtrl = require('../controllers/articles');
-const articlesPopularCtrl = require('../controllers/articles');
+const articlesPopularCtrl = require('../controllers/popular_articles');
 const auth = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/:section', auth, articlesCtrl.getArticles);
-router.get('/popular', auth, articlesCtrl.getPopularArticles);
+router.get('/popular', auth, articlesPopularCtrl.getArticles);
 
 module.exports = router;
